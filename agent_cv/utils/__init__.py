0fd913452207cf3226456f05dcfd921b0e3e0502
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-07-08 21:03:00 -0700

    feat(utils): create_logger

--- agent_cv/utils/__init__.py
+++ agent_cv/utils/__init__.py
@@ -2,4 +2,5 @@
 Utilities for the package (not agents).
 """
 
-from .formatting import normalize_markup
+from .formatting import normalize_markup as normalize_markup
+from .logs import create_logger as create_logger
